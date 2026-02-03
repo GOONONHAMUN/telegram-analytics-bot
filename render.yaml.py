340ddce5d5b5daf923ec93d9349ab0f76f1400c2
GOONONHAMUN <standkiks3@gmail.com>
2026-02-04 02:56:29 +0500

    Update render.yaml.py

--- render.yaml.py
+++ render.yaml.py
@@ -1,11 +1,16 @@
 services:
   - type: web
-    name: telegram-analytics-bot
+    name: telegram-bot-simple
     env: python
     region: frankfurt
     plan: free
     buildCommand: pip install -r requirements.txt
-    startCommand: python app.py  # ВАЖНО: python app.py, не gunicorn!
+    startCommand: python app.py
     healthCheckPath: /health
+    healthCheckTimeout: 180
     autoDeploy: true
-
+    envVars:
+      - key: BOT_TOKEN
+        sync: false
+      - key: PORT
+        value: "10000"
